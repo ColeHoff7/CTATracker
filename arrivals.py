51cee7d56a23b2c501baa1acfb8b5ac6ab0ea64e
Cole Hoffbauer <colehoff7@gmail.com>
2022-05-06 16:11:24 -0500

    catching errors in getting arrivals

--- arrivals.py
+++ arrivals.py
@@ -21,21 +21,25 @@ class Arrivals():
         return round((datetime.fromisoformat(arrival["arrT"]) - datetime.now()).total_seconds() / 60)
     
     def getArrivalTimes(self):
-        southportArrivals = json.loads(requests.get(f'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={self.key}&mapid={self.southportId}&outputType=JSON&rt=Brn').text)['ctatt']['eta']
-        belmontArrivals = json.loads(requests.get(f'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={self.key}&mapid={self.belmontId}&outputType=JSON&rt=Red').text)['ctatt']['eta']
         southportTimes = []
         belmontTimes = []
+        try:
+            southportArrivals = json.loads(requests.get(f'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={self.key}&mapid={self.southportId}&outputType=JSON&rt=Brn').text)['ctatt']['eta']
+            belmontArrivals = json.loads(requests.get(f'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={self.key}&mapid={self.belmontId}&outputType=JSON&rt=Red').text)['ctatt']['eta']
 
-        for belArrival in belmontArrivals:
-            mins = self.getMinsTilArrival(belArrival)
-            if mins > self.timeToWalkToBelmont: 
-                if belArrival["destNm"] == '95th/Dan Ryan':
-                    belArrival["destNm"] = '95th/DR'
-                belmontTimes.append(f'{belArrival["destNm"]} {mins}min')
+            for belArrival in belmontArrivals:
+                mins = self.getMinsTilArrival(belArrival)
+                if mins > self.timeToWalkToBelmont: 
+                    if belArrival["destNm"] == '95th/Dan Ryan':
+                        belArrival["destNm"] = '95th/DR'
+                    belmontTimes.append(f'{belArrival["destNm"]} {mins}min')
 
-        for spArrival in southportArrivals:
-            mins = self.getMinsTilArrival(spArrival)
-            if mins > self.timeToWalkToSouthport: 
-                southportTimes.append(f'{spArrival["destNm"]} {mins}min')
+            for spArrival in southportArrivals:
+                mins = self.getMinsTilArrival(spArrival)
+                if mins > self.timeToWalkToSouthport: 
+                    southportTimes.append(f'{spArrival["destNm"]} {mins}min')
+        except Exception as err:
+            print(err)
+        finally:
+            return [southportTimes, belmontTimes]
 
-        return [southportTimes, belmontTimes]
